9d8472a74117f655690185de5c521297dbe6be64
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-09-04 21:05:21 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello
+{
+ public static void main(String args[])
+{
+ System.out.println("Hello!");
+ System.out.println("Welcome to the world of Java.");
+ System.out.println("Let us learn Java");
+}
+}
