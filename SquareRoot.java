5a56c41ef344c84ea105a6ae739ae8b05b78f40b
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-09-05 10:05:57 +0530

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,12 @@
+//this code computes square root
+import java.lang.Math;
+class Squareroot
+{
+ public static void main(String args[])
+  {
+    double x=5;
+    double y;
+    y = Math.sqrt(x);
+    System.out.println("Y= "+y);
+  }
+}
