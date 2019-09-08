d7ffe1b02c26744db58b1a5e1bfb5676785a8fb1
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-09-08 11:12:41 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+
+public class Sum
+{
+ public static void main(String[] args)
+ {
+   int s=0;
+   for(int i=0;i<args.length;i++)
+   s=s+Integer.parseInt(args[i]);
+
+   System.out.println("Sum is : "+s);
+ }
+}
