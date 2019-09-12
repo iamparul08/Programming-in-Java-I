63e394d617d5702eb9dd3fe34d609a122abcff36
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-09-12 15:39:35 +0530

    base class of Student1

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person
+{
+  private int age;
+  private String name;
+  
+  public void setAge(int a)
+  { age = a; }
+  public void setName(String n)
+  { name=n; }   
+  
+  public int getAge()
+   {return (age); }
+  public String getName()
+   {return (name); }
+}
