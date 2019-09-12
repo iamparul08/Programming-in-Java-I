36e0178dc29008ad2db2580ce96682f33b9df311
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-09-12 10:50:11 +0530

    printing dimensions of a box

--- Box.java
+++ Box.java
@@ -2,11 +2,7 @@ class Box
 {
  private int length,breadth,height;
  public void setDimension(int l, int b, int h)
- {
-   length=l;
-   breadth=b;
-   height=h;
- }
+ { length=l; breadth=b; height=h; }
  public void showDimension()
  {
    System.out.println("L= "+length);
