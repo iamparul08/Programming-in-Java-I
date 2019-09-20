b469c5c610b212aafc38162d04208b4b95b61fba
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-09-20 07:42:12 +0530

    Multilevel inheritance

---
+++ Multilevel.java
@@ -0,0 +1,38 @@
+//multilevel inheritance
+class Person
+{
+  private int age;
+  private String name;
+  
+  public void setAge(int a)
+  { age = a; }
+  public void setName(String n)
+  { name=n; }   
+  
+  public int getAge()
+   {return (age); }
+  public String getName()
+   {return (name); }
+}
+class Student1 extends Person
+{
+   private int rollno;
+   public void setRollno(int r)
+     {rollno = r; }
+    public int getRollno()
+     { return (rollno); }
+}
+class Multilevel
+{
+  public static void main(String[] args)
+   {
+     Student1 s=new Student1();
+      s.setRollno(456);
+      s.setName("Dolly");
+      s.setAge(34);
+      
+      System.out.println("Roll no: "+s.getRollno());
+      System.out.println("Name: "+s.getName());
+      System.out.println("Age is: "+s.getAge());
+   }
+}
