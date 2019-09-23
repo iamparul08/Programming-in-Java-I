50b17aba15c67f3398e81079fc208f0b50c3d68d
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-09-23 15:46:54 +0530

    Nesting of methods

---
+++ Nesting.java
@@ -0,0 +1,30 @@
+//nesting of methods
+class NestingTest
+{
+	int m,n;
+	NestingTest(int x,int y)	//construcor method
+	{
+		m=x;
+		n=y;
+	}
+	int largest()
+	{
+		if(m>=n)
+			return(m);
+		else
+			return(n);
+	}
+	void display()
+	{
+		int large=largest();
+		System.out.println("Lasrgest value= "+large);
+	}
+}
+class Nesting
+{
+	public static void main(String []args)
+	{
+		NestingTest nest=new NestingTest(50,40);
+		nest.display();
+	}
+}
