35c33061d9ec63c20dcbd70e86dd9a616515f810
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-10-05 10:59:01 +0530

    an another example to threading

---
+++ RunThread.java
@@ -0,0 +1,31 @@
+//3. Debugged the code
+class multi1 extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println("\tFrom Thread 1 i= "+i);
+		}
+	}
+}
+class multi2 extends Thread
+{
+	public void run()
+	{
+		for(int j=1;j<=5;j++)
+		{
+			System.out.println("\tFrom Thread 2 j= "+j);
+		}
+	}
+}
+class RunThread
+{
+	public static void main(String[] args)
+	{
+		multi1 m1 = new multi1();
+		multi2 m2 = new multi2();
+		m1.start();
+		m2.start();
+	}
+}
