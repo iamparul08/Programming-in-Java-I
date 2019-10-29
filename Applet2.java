d6c49e2b07e8c44cc532a5ef09aa958db90f4add
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-10-29 11:41:40 +0530

    2. paint() method in Applet

---
+++ Applet2.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.awt.*;
+/* <applet code="MyApplet" width="300" height="300"></applet> */
+public class Applet2 extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawString("Parul",200,200);
+	}
+}
