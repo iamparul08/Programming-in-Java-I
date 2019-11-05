3ca5a4207b6f0ebbbbf8d8260dd135031771e782
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-11-05 20:32:25 +0530

    First program in Swing

---
+++ Swing1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class Swing1
+{
+	public static void main(String[] args)
+	{
+		JFrame jf = new JFrame("Swing Example");
+		jf.setSize(300,300);
+		jf.setVariable(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
